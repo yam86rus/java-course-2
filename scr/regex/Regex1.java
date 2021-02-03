241a8657e59f3b778fb544071412a1e6f182597b
Yam86rus <yam86rus@gmail.com>
2021-02-03 16:26:07 +0500

    up

---
+++ scr/regex/Regex1.java
@@ -0,0 +1,10 @@
+package regex;
+
+public class Regex1 {
+    public static void main(String[] args) {
+        String s =" Ivanov vasily, Russia, Moscow, Lenin Street, 51, Flat 48;"+
+                "email:vivanov@mail.ru, Postcode:AA99, Phone Number: +1234556" +
+                "Petrova Marya, Ukraine,Kiev, Lomonosov street, 33, Flat 18,"+
+                "email:masha@gmail.com, Postcode:UKR54,Phone number: +987987987";
+    }
+}
---
+++ scr/regex/Regex2.java
@@ -0,0 +1,29 @@
+package regex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Regex2 {
+    public static void main(String[] args) {
+//        String s1 = "ABCD ABCE ABCFABCGABCH";
+//        String s1 = "LHNAJBGBFCFD";
+//        String s1 = "ABDOP";
+//        String s1 = "abcd abce abc5abcg6abch";
+        String s1 = "abcd abce abc5abcg6abch";
+//        Pattern pattern1 = Pattern.compile("abc(d|5)");
+//        Pattern pattern1 = Pattern.compile("abc.");
+//        Pattern pattern1 = Pattern.compile("^abc");
+//        Pattern pattern1 = Pattern.compile("abc5abcg6abch$");
+//        Pattern pattern1 = Pattern.compile("[0-9]");
+//        Pattern pattern1 = Pattern.compile("\\d");
+        Pattern pattern1 = Pattern.compile("\\D");
+
+
+
+        Matcher matcher = pattern1.matcher(s1);
+
+        while (matcher.find()){
+            System.out.println("possition " + matcher.start() + "  "  + matcher.group());
+        }
+    }
+}
